a9acc67d5f13e83b6d0e8735070e29ef7b4d8ee8
Matthew Harris <matthewharrisak@gmail.com>
2020-10-02 11:15:00 -0500

    WE've GOT BUTTONS

--- scripts.js
+++ scripts.js
@@ -1,8 +1,43 @@
 console.log('JS');
+let favFoods = ["rice" , "kimchi" , " soysauce" , "peanutBUTTER" , "ICECREAM"];
 $(document).ready(onReady);
+
+
 
 function onReady(params) {
     console.log('Hello from Jquery');
+    $('#clickMe').on('click' , buttonClicked);
+
+    // descendent selector
+    $('#favoriteFoods').on('click' , '.deleteBtn' , deleteFunc);
+    $('#favoriteFoods').on('click' , '.changeColor' , changeColor);
+
+    
+}    
+
+function deleteFunc() {
+    console.log('delete');
+    $(this).parent().remove();
+}    
+
+function changeColor() {
+    $(this).parent().toggleClass('pinkColor');
+}
+// this = delete btn
+//the .parent() = the <li>
+// .remove() removes that element
+
+function buttonClicked(){
+    let ulFood = $("#favoriteFoods") 
+    for (let i = 0; i < favFoods.length; i++){
+    ulFood.append(
+        `<li> one of my favorite foods is ${favFoods[i]} 
+            <button class="deleteBtn">DELETE</button> 
+            <button class="changeColor">Change Color</button> 
+        </li>`);
+    }
+    
+        console.log("IM CLICKED");
 }
 
 // event #1
@@ -10,7 +45,5 @@ onReady();
 
 onReady();    
 
-Function gitTest(){
-    console.log('we are commiting to a new repro');
     
-}
+
